b45df1439bb34c60f8fdd9a7eb4618b322fdfc91
jose.sandoval.tirado <jose.sandoval.tirado@bbva.com>
2024-01-29 15:16:41 -0500

    PERAR-25814: FEAT!: validate bbva customer participant of types person and legal person

---
+++ artifact/transactions/RBVDT041-01-PE/src/main/java/com/bbva/rbvd/AbstractRBVDT04101PETransaction.java
@@ -0,0 +1,29 @@
+package com.bbva.rbvd;
+
+import com.bbva.elara.transaction.AbstractTransaction;
+import com.bbva.rbvd.dto.insurance.commons.ParticipantsDTO;
+import java.util.List;
+
+/**
+ * In this class, the input and output data is defined automatically through the setters and getters.
+ */
+public abstract class AbstractRBVDT04101PETransaction extends AbstractTransaction {
+
+	public AbstractRBVDT04101PETransaction(){
+	}
+
+
+	/**
+	 * Return value for input parameter participants
+	 */
+	protected List<ParticipantsDTO> getParticipants(){
+		return (List<ParticipantsDTO>)this.getParameter("participants");
+	}
+
+	/**
+	 * Return value for input parameter quotation-id
+	 */
+	protected String getQuotationId(){
+		return (String)this.getParameter("quotation-id");
+	}
+}
